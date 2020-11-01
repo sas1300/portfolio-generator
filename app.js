7c532e3617656c015dee00d0e4c132513ea7b285
sas1300 <sascode13@gmail.com>
2020-11-01 14:30:00 -0600

    updated html template added feature sections

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
-// const fs =require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs =require('fs');
+const generatePage = require('./src/page-template.js');
+
 
 // const pageHTML = generatePage(name, github);
 
@@ -145,6 +146,11 @@ const promptUser = () => {
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-  });
-  
+     const pageHTML = generatePage(portfolioData);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw new Error (err);
+
+console.log('Portfolio complete! Check out index.html to see the output!');
+  });  
+});
